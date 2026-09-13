7a80486dc3ba3549d924714be502f6bd43eeaaef
PHCN <pedrohenrique.cn07@gmail.com>
2026-09-13 20:55:17 -0300

    feat: Finalização do arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Integrantes from "./pages/Integrantes";
+import Faq from "./pages/Faq";
+import SobreSouly from "./pages/SobreSouly";
+import SoulyComoGuia from "./pages/SoulyComoGuia";
+import Contato from "./pages/Contato";
+import NaoEncontrada from "./pages/NaoEncontrada";
+
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="flex min-h-screen flex-col bg-slate-50 font-sans text-slate-700">
+      <a
+        href="#conteudo"
+        className="sr-only z-50 bg-teal-950 p-4 text-white focus:not-sr-only focus:absolute"
+      >
+        Pular para o conteúdo
+      </a>
+      <Header />
+
+      {/* Apenas o conteúdo das rotas muda. Header e Footer são compartilhados. */}
+      <main id="conteudo" tabIndex={-1} className="flex-1 focus:outline-none">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/integrantes" element={<Integrantes />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/sobre-souly" element={<SobreSouly />} />
+          <Route path="/souly-como-guia" element={<SoulyComoGuia />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="*" element={<NaoEncontrada />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
