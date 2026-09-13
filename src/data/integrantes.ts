0c7034529dcd59a8e102993309914e6ba91bfe70
KauaBF <kauabferreiraa@gmail.com>
2026-09-13 20:33:35 -0300

    Feat: Criação do aqrquivo integrantes.ts na pasta Data contendo informações como nome, rm, foto, github e linkedin

---
+++ src/data/integrantes.ts
@@ -0,0 +1,37 @@
+export const integrantes = [
+  {
+    nome: "Gabriel Pereira de Oliveira",
+    rm: "572262",
+    foto: "/images/gabriel.jpg",
+    github: "https://github.com/Gabriel-Oliveira0611",
+    linkedin: "https://www.linkedin.com/in/gabriel-pereira-1aa4bb224/",
+  },
+  {
+    nome: "Adalto Massahiro Nagabe",
+    rm: "572298",
+    foto: "/images/adalto.jpg",
+    github: "https://github.com/AdaltoNagabe",
+    linkedin: "https://www.linkedin.com/in/adalto-massashiro-nagabe-149685104/",
+  },
+  {
+    nome: "Pedro Henrique Carvalho",
+    rm: "570492",
+    foto: "/images/pedro.jpg",
+    github: "https://github.com/pedrohcnascimento",
+    linkedin: "https://www.linkedin.com/in/pedrohenriquecn07/",
+  },
+  {
+    nome: "Murillo Siviero Lopes",
+    rm: "572724",
+    foto: "/images/murillo.jpg",
+    github: "https://github.com/MurilloSLopes",
+    linkedin: "https://www.linkedin.com/in/murillo-llopes-23out99/",
+  },
+  {
+    nome: "Kauã Barros Ferreira",
+    rm: "571801",
+    foto: "/images/kaua.jpg",
+    github: "https://github.com/KauaBF",
+    linkedin: "https://www.linkedin.com/in/kau%C3%A3-barros-ferreira-161a3a36b/",
+  },
+];
